c301072a8c5eb68f76fc5dee53fd72ce9a313b37
Doug Rodriguez <drodriguez@cubog.co>
2020-10-25 00:10:11 -0400

    Fixing input spacing in form

--- src/components/EditView/EditView.js
+++ src/components/EditView/EditView.js
@@ -1,12 +1,9 @@
 import React, { useState, useContext } from "react";
-import PropTypes from "prop-types";
 import "./EditView.css";
-import Nav from "../Nav/Nav";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 import FormInput from "../FormInput/FormInput";
 import DateInput from "../DateInput/DateInput";
-import { useForm, Controller } from "react-hook-form";
-import { MDBFileInput } from "mdbreact";
+import { useForm } from "react-hook-form";
 import { store } from "../../providers/ListProviders";
 
 const EditView = ({ title, history }) => {
@@ -65,12 +62,10 @@ const EditView = ({ title, history }) => {
               items={items}
               register={register}
             />
-            <br />
-
             <div style={{ display: "flex", flexDirection: "column" }}>
               <label className="grey-text">Image</label>
               <input
-                style={{ marginBottom: 20, width: 100 }}
+                style={{ marginBottom: 10, width: 100 }}
                 type="file"
                 onChange={onImageChange}
                 ref={register}
@@ -83,11 +78,9 @@ const EditView = ({ title, history }) => {
                 </MDBRow>
               )}
             </div>
-            <br />
             <label className="grey-text">Pick-up Window</label>
             <DateInput title="From" register={register} name="from" />
             <DateInput title="Until" register={register} name="until" />
-            <br />
             <FormInput title="Address" id="address" register={register} />
             <div className="text-center mt-4">
               <MDBBtn

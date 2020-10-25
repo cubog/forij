6244ae1da2b6a730e037ea78a51606563e20d096
Doug Rodriguez <drodriguez@cubog.co>
2020-10-24 21:50:45 -0400

    Fixing scroll for form

--- src/components/EditView/EditView.js
+++ src/components/EditView/EditView.js
@@ -14,46 +14,41 @@ const EditView = ({ title }) => {
     console.log(data);
   };
   return (
-    <div className="EditView">
+    <div className="EditView" style={{ paddingBottom: 70 }}>
       <MDBContainer>
-        <MDBRow>
-          <MDBCol md="6">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <p className="h4 text-center mb-4">
-                {title || "Create donation"}
-              </p>
-              <FormInput
-                title="Donation Items"
-                id="item"
-                addInput={addItem}
-                items={items}
-                register={register}
+        <MDBCol md="6">
+          <form onSubmit={handleSubmit(onSubmit)} style={{ paddingTop: 10 }}>
+            <p className="h4 text-center mb-4">{title || "Create donation"}</p>
+            <FormInput
+              title="Donation Items"
+              id="item"
+              addInput={addItem}
+              items={items}
+              register={register}
+            />
+            <br />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <label className="grey-text">Image</label>
+              <input
+                style={{ marginBottom: 20, width: 150 }}
+                type="file"
+                name="image"
               />
-              <br />
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <label className="grey-text">Image</label>
-                <input
-                  style={{ marginBottom: 20, width: 150 }}
-                  type="file"
-                  // value="Upload Image"
-                  name="image"
-                />
-              </div>
-              <br />
-              <label className="grey-text">Pick-up Window</label>
-              <DateInput title="From" register={register} name="from" />
-              <DateInput title="Until" register={register} name="until" />
-              <br />
-              <FormInput title="Address" id="address" register={register} />
-              <div className="text-center mt-4">
-                <MDBBtn color="red">Cancel</MDBBtn>
-                <MDBBtn color="indigo" type="submit">
-                  Save
-                </MDBBtn>
-              </div>
-            </form>
-          </MDBCol>
-        </MDBRow>
+            </div>
+            <br />
+            <label className="grey-text">Pick-up Window</label>
+            <DateInput title="From" register={register} name="from" />
+            <DateInput title="Until" register={register} name="until" />
+            <br />
+            <FormInput title="Address" id="address" register={register} />
+            <div className="text-center mt-4">
+              <MDBBtn color="red">Cancel</MDBBtn>
+              <MDBBtn color="indigo" type="submit">
+                Save
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
       </MDBContainer>
     </div>
   );
